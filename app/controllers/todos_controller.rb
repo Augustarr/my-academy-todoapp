711dc46d85f750b4fa0669d12edc8b9659f4c582
Nawamin Wongpakham <nawamin@MacBook-Air-khxngNawamin.local>
2025-08-20 10:32:34 +0700

    feat: Add todo controller for new, update and destroy

--- app/controllers/todos_controller.rb
+++ app/controllers/todos_controller.rb
@@ -1,5 +1,60 @@
 class TodosController < ApplicationController
+   before_action :set_todo, only: [ :show, :edit, :update, :destroy ]
+
   def index
-    @todos = Todo.all
+    @todos = Todo.all.order(created_at: :desc)
+  end
+
+  def show
+  end
+
+  def new
+    @todos = Todo.new
+  end
+
+  def create
+   @todo = Todo.new(todo_params)
+
+    respond_to do |format|
+      if @todo.save
+        format.turbo_stream
+        format.html { redirect_to todo_url(@todo), notice: "Todo was successfully created." }
+      else
+        format.turbo_stream { render turbo_stream: turbo_stream.replace("#{helpers.dom_id(@todo)}_form", partial: "form", locals: { todo: @todo }) }
+        format.html { render :new, status: :unprocessable_entity }
+      end
+    end
+  end
+
+  def edit; end
+
+  def update
+    respond_to do |format|
+      if @todo.update(todo_params)
+        format.turbo_stream { render turbo_stream: turbo_stream.replace("#{helpers.dom_id(@todo)}", partial: "todo", locals: { todo: @todo }) }
+        format.html { redirect_to root_path, notice: "Todo updated successfully." }
+      else
+        format.html { render :edit }
+      end
+    end
+  end
+
+  def destroy
+   @todo.destroy!
+
+    respond_to do |format|
+      format.turbo_stream { render turbo_stream: turbo_stream.remove("#{helpers.dom_id(@todo)}") }
+      format.html { redirect_to todos_path, status: :see_other, notice: "Todo was successfully destroyed." }
+    end
+  end
+
+  private
+
+  def set_todo
+    @todo = Todo.find(params[:id])
+  end
+
+  def todo_params
+    params.require(:todo).permit(:title, :completed)
   end
 end
--- test/controllers/todos_controller_test.rb
+++ test/controllers/todos_controller_test.rb
@@ -1,7 +1,25 @@
 require "test_helper"
 
 class TodosControllerTest < ActionDispatch::IntegrationTest
-  # test "the truth" do
-  #   assert true
-  # end
+  fixtures :todos
+  test "Should_be_visit_todolist_page" do
+    get todos_path
+    assert_response :success
+  end
+
+  test "Should_be_create_todo" do
+    assert_difference("Todo.count", 1) do
+      post todos_path, params: {
+        todo: {
+          title: "new_todo",
+          completed: false
+        }
+      }
+    end
+
+    todo = Todo.last
+    assert_equal "new_todo", todo.title
+    assert_not todo.completed
+    assert_redirected_to todo_path(todo)
+  end
 end
