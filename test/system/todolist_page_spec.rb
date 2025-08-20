7bb77027b26affd1566ecdb8d8389c5204d523fd
Nawamin Wongpakham <nawamin@MacBook-Air-khxngNawamin.local>
2025-08-20 15:41:13 +0700

    test: Add system test for ckeckbox and mybragdocBtn

--- test/system/todolist_page_spec.rb
+++ test/system/todolist_page_spec.rb
@@ -14,4 +14,24 @@ RSpec.describe "todolist page", type: :feature, js: true do
     find("[data-testid='todo-id-#{ todo.id }-del-btn']").click
     expect(page).to have_no_selector("[data-testid='todo-title-id-#{todo.id}']")
   end
+
+  it "Given_UserVisitTodoListPage_When_UserUncheckCheckbox_Then_TodoItemHasCompleted" do
+    todo = Todo.create(title: "test-todo-item", completed: false)
+    visit "/todos"
+    find("[data-testid='todo-id-#{ todo.id }-uncheck-checkbox']").click
+    expect(page).to have_selector("[data-testid='todo-id-#{ todo.id }-check-checkbox']")
+  end
+
+  it "Given_UserVisitTodoListPage_When_UserCheckCheckbox_Then_TodoItemHasNotCompleted" do
+    todo = Todo.create(title: "test-todo-item", completed: true)
+    visit "/todos"
+    find("[data-testid='todo-id-#{ todo.id }-check-checkbox']").click
+    expect(page).to have_selector("[data-testid='todo-id-#{ todo.id }-uncheck-checkbox']")
+  end
+
+  it "Given_UserVisitTodoListPage_When_UserChickMyBragDocumentBtn_Then_UserCanVisitMyBragDocumentPage" do
+    visit "/todos"
+    find("[data-testid='my-brag-doc-btn']").click
+    expect(page).to have_current_path("/profile")
+  end
 end
